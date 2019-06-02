4723794d9555fe6a61664fe19d56cdf51f9e1cfe
Bernado Baumblatt <baumblatt@gmail.com>
2019-06-02 16:28:33 -0300

    Flexbox - Basics & Terminology - Container

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {HomeComponent} from './containers/home/home.component';
 import {LayoutComponent} from './containers/layout/layout.component';
+import {FlexboxComponent} from './lessons/01-flexbox/flexbox/flexbox.component';
 
 const routes: Routes = [
 	{path: '', pathMatch: 'full', redirectTo: 'layout'},
 	{
 		path: 'layout', component: LayoutComponent, children: [
 			{path: '', pathMatch: 'full', redirectTo: 'home'},
-			{path: 'home', component: HomeComponent}
+			{path: 'home', component: HomeComponent},
+			{path: 'flexbox', component: FlexboxComponent},
 		]
 	}
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,36 @@
 import {FlexModule} from '@angular/flex-layout';
 import {MatButtonModule, MatIconModule, MatListModule, MatSidenavModule, MatTableModule, MatToolbarModule} from '@angular/material';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LayoutComponent } from './containers/layout/layout.component';
-import { HomeComponent } from './containers/home/home.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutComponent} from './containers/layout/layout.component';
+import {HomeComponent} from './containers/home/home.component';
+import {FlexboxComponent} from './lessons/01-flexbox/flexbox/flexbox.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LayoutComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatTableModule,
-    MatButtonModule,
-    MatIconModule,
-    MatListModule,
-    FlexModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+	declarations: [
+		AppComponent,
+		LayoutComponent,
+		HomeComponent,
+		FlexboxComponent
+	],
+	imports: [
+		BrowserModule,
+		AppRoutingModule,
+		BrowserAnimationsModule,
+		MatSidenavModule,
+		MatToolbarModule,
+		MatTableModule,
+		MatButtonModule,
+		MatIconModule,
+		MatListModule,
+		FlexModule
+	],
+	providers: [],
+	bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
